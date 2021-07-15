96cd7c45889464d93f5e4e4b1c5490841dcdbee3
Fuad Muhammed <62259225+G4me8woy@users.noreply.github.com>
2021-07-15 18:35:28 +0000

    validation update
    no REGEX

--- Authentication/login.php
+++ Authentication/login.php
@@ -1,3 +1,30 @@
+<?php   
+    $errors = array(
+        'auth-id' => "", 
+        'password' => ""
+    );
+    
+    $fields = array(
+        'auth-id' => "", 
+        'password' => ""
+    );
+
+    if (isset($_POST['button'])) {
+        //accepting input
+        $fields['auth-id'] = htmlspecialchars($_POST['auth-id']);
+        $fields['password'] =htmlspecialchars( $_POST['password']);
+
+        //validating emptiness
+        if (empty($fields['auth-id'])) {
+            $errors['auth-id'] = "an id is required"; 
+        }
+        if (empty($fields['password'])) {
+            $errors['password'] = " a password is required";
+        }
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,23 +45,26 @@
             color: blue;
             text-decoration: none;
         }
+        .error{
+            color: red;
+        }
     </style>
 </head>
 <body>
     <form action="" method="post" id="form">
         <label>
             Patient / Staff ID:
-            <input type="text" name="auth-id" id="auth-id">
+            <input type="text" name="auth-id" id="auth-id" value="<?php echo $fields['auth-id'] ?>" > <br>
+            <div class="error"><?php  echo $errors['auth-id'] ?></div>
         </label>
         <label>
             Password:
-            <input type="password" name="password" id="password">
+            <input type="password" name="password" id="password" value="<?php echo $fields['password'] ?>"> <br>
+            <div class="error"><?php  echo $errors['password'] ?></div>
         </label>
-        <label>
-            <input type="button" value="Log In">
-        </label>
+            <input type="submit" value="Log In" name="button">
     </form>
 
-    <p>new here ? <a href="#" id="sign-up-link">Register</a> </p>
+    <p>new here ? <a href="./register.php" id="sign-up-link">Register</a> </p>
 </body>
 </html>
--- Authentication/register.php
+++ Authentication/register.php
@@ -1,5 +1,76 @@
+
+<?php
+
+$errors = array(
+    'firstName' => "", 
+    'lastName' => "",
+    'email' => "",
+    'contact' => "",
+    'password-1' => "",
+    'password-2' => "",
+);
+
+$fields = array(
+    'firstName'=>"",
+    'lastName' => "", 
+    'email' => "", 
+    'contact' => "", 
+    'password-1' => "", 
+    'password-2' => ""
+);
+
+if (isset($_POST['submit'])) {
+    //getting data form input fields
+    $firstName = htmlspecialchars($_POST['first-name']);
+    $lastName = htmlspecialchars($_POST['last-name']);
+    $email = htmlspecialchars($_POST['email']);
+    $contact = htmlspecialchars($_POST['contact']);
+    $password_1 = htmlspecialchars($_POST['password-1']);
+    $password_2 = htmlspecialchars($_POST['password-2']);
+
+    
+    //storing data in fields array
+    $fields['firstName'] = $firstName;
+    $fields['lastName'] = $lastName; 
+    $fields['email'] = $email;
+    $fields['contact'] = $contact;
+    $fields['password-1'] = $password_1; 
+    $fields['password-2'] = $password_2;
+
+
+    //checking for empty fields
+    foreach ($fields as $field => $value) {
+        if (empty($value)) {
+            if ($field == "password-1" or $field == "password-2") {
+                $errors[$field] = "password can't be empty";
+            }else {
+                $errors[$field] = "$field can't be empty";
+            }
+        }
+    }
+
+
+    //further validation
+    foreach ($errors as $field => $error) {
+        if ( $field == "email" and empty($error)) {
+            
+                if (!filter_var($fields[$field], FILTER_VALIDATE_EMAIL)) {
+                   $errors[$field] = "invalid email address"; 
+                // echo "invalid email";
+            }
+        }
+    }
+    
+
+}
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,52 +78,63 @@
     <title>Register</title>
 
     <style>
-        #form{
+        #form {
             display: flex;
             flex-direction: column;
         }
+
         #form label {
             margin-bottom: 20px;
         }
-        a{
+
+        a {
             color: blue;
             text-decoration: none;
         }
+
+        .error {
+            color: red;
+        }
     </style>
 </head>
+
 <body>
     <form action="register.php" method="post" id="form">
         <label>
             First Name:
-            <input type="text" name="first-name" id="first-name">
+            <input type="text" name="first-name" id="first-name" value="<?php echo $fields['firstName'] ?>"> <br>
+           <div class="error"><?php echo $errors['firstName'] ?></div>
         </label>
-        
         <label>
             Last Name:
-            <input type="text" name="last-name" id="last-name">
+            <input type="text" name="last-name" id="last-name" value="<?php echo $fields['lastName'] ?>"> <br>
+            <div class="error"><?php echo $errors['lastName'] ?></div>
         </label>
         <label>
             Email:
-            <input type="email" name="email" id="email">
+            <input type="text" name="email" id="email" value="<?php echo $fields['email'] ?>"><br>
+            <div class="error"><?php echo $errors['email'] ?></div>
         </label>
         <label>
             Contact:
-            <input type="tel" name="contact" id="contact">
+            <input type="tel" name="contact" id="contact" value="<?php echo $fields['contact'] ?>"><br>
+            <div class="error"><?php echo $errors['contact'] ?></div>
         </label>
         <label>
             Password:
-            <input type="password" name="password-1" id="password-1">
+            <input type="password" name="password-1" id="password-1" ><br>
+            <div class="error"><?php echo $errors['password-1'] ?></div>
         </label>
         <label>
             Confirm Password:
-            <input type="password" name="password-2" id="password-2">
+            <input type="password" name="password-2" id="password-2" ><br>
+            <div class="error"><?php echo $errors['password-2'] ?></div>
         </label>
 
-        <label>
-            <input type="submit" value="Register">
-        </label>
+        <input type="submit" name="submit" value="Register">
     </form>
 
-    <p>already have an account ? <a href="#" id="sign-in-link">Sign In</a> </p>
+    <p>already have an account ? <a href="./login.php" id="sign-in-link">Sign In</a> </p>
 </body>
+
 </html>
